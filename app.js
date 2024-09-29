d3bdb6a540a90f815c886e6dc20987098e8430b5
MhuhammadAbdullah <abaloch0033@gmail.com>
2024-09-29 09:26:10 +0500

    updated

---
+++ app.js
@@ -0,0 +1,19 @@
+// Question: 01
+
+
+// var seriesArray = [1, 2, 3, 4, 5, 6, 7, 9, 10];
+
+// var totalNumbers = 10;
+// var expectedSum = (totalNumbers * (totalNumbers + 1)) / 2;
+// var actualSum = 0;
+
+// for (var i = 0; i < seriesArray.length; i++) {
+//     actualSum += seriesArray[i];
+// }
+
+// var missingNumber = expectedSum - actualSum;
+
+// console.log("The missing number is:", missingNumber);
+
+
+
